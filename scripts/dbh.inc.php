afe64479583d1cf317438cd499e601982f64e91a
Achintya <achintyasi@gmail.com>
2018-11-18 12:35:47 -0500

    added db config

--- scripts/dbh.inc.php
+++ scripts/dbh.inc.php
@@ -1,13 +1,13 @@
 <?php
 
-$servername = "localhost";
-$dbUsername = "pmauser";
-$dbPassword = "Achintya2";
-$dbName = "ta_info";
+$servername = "us-cdbr-iron-east-01.cleardb.net";
+$dbUsername = "b9b0bab205ee44";
+$dbPassword = "4268dd78";
+$dbName = "heroku_0671d1b843b6769";
 
 $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
 if (!$conn) {
     die("Connection Failed! ".mysqli_connect_error());
-    
+
 }
